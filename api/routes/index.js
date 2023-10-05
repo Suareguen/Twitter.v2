edb5f0ec7e37913262321f12dca5580f7b273ab4
Suarenguen <adrian.suarez.monzon@gmail.com>
2023-10-05 08:51:29 +0100

    General: - Signup and login implemented

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,6 +3,8 @@ const router = require('express').Router()
 
 router.use('/user', require('./user.router.js'))
 router.use("/post", require("./post.router.js"))
+router.use("/auth", require("./auth.router.js"));
+
 
 
 
